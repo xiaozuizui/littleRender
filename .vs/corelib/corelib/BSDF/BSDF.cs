a9932e0e5822b81769e2b1009280d8315b8bac75
立璟 赵 <785897146@qq.com>
2017-09-13 09:12:23 +0800

    new

---
+++ .vs/corelib/corelib/BSDF/BSDF.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace corelib.BSDF
+{   
+     public class Color
+    {
+        public Color() { R = 0;G = 0;B = 0; }
+        public Color(float r,float g,float b)
+        { R = r;G = g;B = b; }
+        float R { get; set; }
+        float G { get; set; }
+        float B { get; set; }
+    }
+    public class BSDF
+    {
+
+        private Color rgb ;
+        private float Reflectiuon { get; set; }
+        private float Diffuse { get; set; }
+        
+
+
+
+        public void SetColor(float r ,float g,float b)
+        {
+            
+        }
+    }
+}
