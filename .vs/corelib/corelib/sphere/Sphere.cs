cf32ec272652ee32a4bdaacf68ee686e2b23527e
立璟 赵 <785897146@qq.com>
2017-09-21 00:37:21 +0800

    new

--- .vs/corelib/corelib/sphere/Sphere.cs
+++ .vs/corelib/corelib/sphere/Sphere.cs
@@ -8,9 +8,26 @@ namespace corelib.sphere
     class Sphere:Shape
     {
         float radius;
-        public override bool Intersect(Ray r,DifferentialGeometry dg)
+        public override bool Intersect(Transform o2w, Ray ray,DifferentialGeometry dg)
         {
+            float A = ray.d.x * ray.d.x + ray.d.y * ray.d.y + ray.d.z * ray.d.z;
+            float B = 2 * (ray.d.x * ray.o.x + ray.d.y * ray.o.y + ray.d.z * ray.o.z);
+            float C = ray.o.x * ray.o.x + ray.o.y * ray.o.y +
+                      ray.o.z * ray.o.z - radius * radius;
 
+            float [] t = new float[2];
+            if (!Quadratic(A, B, C, t))
+                return false;
+
+
+            if (t[0] > ray.maxt || t[1] < ray.mint)
+                return false;
+            float thit = t[0];
+            if (t[0] < ray.mint)
+            {
+                thit = t[1];
+                if (thit > ray.maxt) return false;
+            }
             return true;
         }
     }
