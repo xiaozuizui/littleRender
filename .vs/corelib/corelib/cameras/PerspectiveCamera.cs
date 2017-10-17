fc6bbe1b6d92b8c0d30d10fe73ff9821b8994fd3
littlemm <785897146@qq.com>
2017-10-17 22:32:05 +0800

    update

--- .vs/corelib/corelib/cameras/PerspectiveCamera.cs
+++ .vs/corelib/corelib/cameras/PerspectiveCamera.cs
@@ -52,12 +52,74 @@ namespace corelib.cameras
             CameraToWorld(*ray, ray);
             return 1.f;
         }
-           
+
+        override public float GenerateRayDifferential(CameraSample sample, RayDifferential ray)
+        {
+            // Generate raster and camera samples
+        
+            Point3 Pcamera = RasterToCamera.CaculatePoint(new Point3(sample.imageX, sample.imageY, 0));
+
+            Vector dir = Normalize(new Vector(Pcamera.x, Pcamera.y, Pcamera.z));
+            ray = new RayDifferential(new Point3(0, 0, 0), dir, 0.0f, INFINITY);
+            // Modify ray for depth of field
+            if (lensRadius > 0.0)
+            {
+                // Sample point on lens
+                float lensU, lensV;
+                ConcentricSampleDisk(sample.lensU, sample.lensV, &lensU, &lensV);
+                lensU *= lensRadius;
+                lensV *= lensRadius;
+
+                // Compute point on plane of focus
+                float ft = focalDistance / ray->d.z;
+                Point Pfocus = (*ray)(ft);
+
+                // Update ray for effect of lens
+                ray->o = Point(lensU, lensV, 0.f);
+                ray->d = Normalize(Pfocus - ray->o);
+            }
+
+            // Compute offset rays for _PerspectiveCamera_ ray differentials
+            if (lensRadius > 0.)
+            {
+                // Compute _PerspectiveCamera_ ray differentials with defocus blur
+
+                // Sample point on lens
+                float lensU, lensV;
+                ConcentricSampleDisk(sample.lensU, sample.lensV, &lensU, &lensV);
+                lensU *= lensRadius;
+                lensV *= lensRadius;
+
+                Vector dx = Normalize(Vector(Pcamera + dxCamera));
+                float ft = focalDistance / dx.z;
+                Point pFocus = Point(0, 0, 0) + (ft * dx);
+                ray->rxOrigin = Point(lensU, lensV, 0.f);
+                ray->rxDirection = Normalize(pFocus - ray->rxOrigin);
+
+                Vector dy = Normalize(Vector(Pcamera + dyCamera));
+                ft = focalDistance / dy.z;
+                pFocus = Point(0, 0, 0) + (ft * dy);
+                ray->ryOrigin = Point(lensU, lensV, 0.f);
+                ray->ryDirection = Normalize(pFocus - ray->ryOrigin);
+            }
+            else
+            {
+                ray->rxOrigin = ray->ryOrigin = ray->o;
+                ray->rxDirection = Normalize(Vector(Pcamera) + dxCamera);
+                ray->ryDirection = Normalize(Vector(Pcamera) + dyCamera);
+            }
+
+            ray->time = sample.time;
+            CameraToWorld(*ray, ray);
+            ray->hasDifferentials = true;
+            return 1.f;
+        }
+
 
         //public Transform cam2world { get; set; }
 
 
-        
+
 
         public Vector dxCamera { get; set; }
          public Vector dyCamera { get; set; }
--- .vs/corelib/corelib/core/Render.cs
+++ .vs/corelib/corelib/core/Render.cs
@@ -11,6 +11,7 @@ namespace corelib.core
 
         public  void Run()
         {
+            while(sampler.GetMoreSamples())
             for(int i = 0;i<sampler.SamplePerPixel;i++)
             {
               //  camera.GenerateRay();
