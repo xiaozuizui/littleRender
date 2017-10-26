d46f154aa2e0a81d2f197c423817061dfbf24ad6
littlemm <785897146@qq.com>
2017-10-26 21:35:37 +0800

    update

--- .vs/corelib/corelib/core/Render.cs
+++ .vs/corelib/corelib/core/Render.cs
@@ -4,11 +4,38 @@ using System.Text;
 
 namespace corelib.core
 {
-    class Render
+
+    interface Renderer
+    {
+        void Renderer(Scene scene);
+        RGBSpectrum Li();
+        RGBSpectrum Transmittance();
+
+    }
+    class SampleRenderer:Renderer
     {
         Sampler sampler;
         Camera camera;
+        //Scene
+        SampleRenderer(Scene s)
+        {
 
+        }
+
+        void Renderer(Scene s)
+        {
+
+        }
+
+        RGBSpectrum Li()
+        {
+            return null;
+        }
+
+        RGBSpectrum Transmittance()
+        {
+            return null;
+        }
         public  void Run()
         {
             while(sampler.GetMoreSamples())
---
+++ .vs/corelib/corelib/core/Spectrum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace corelib.core
+{
+    public class RGBSpectrum
+    {
+        float [] color;
+
+        public RGBSpectrum()
+        {
+            color = new float[3] { 0.0f,0.0f,0.0f};
+        }
+        public RGBSpectrum(float[] c)
+        {
+            color = c;
+        }
+    }
+}
