b00b7a98404c7c4a06d5a928cf02986e3d407856
littlemm <785897146@qq.com>
2017-12-02 12:59:47 +0800

    add bsdf

---
+++ .vs/corelib/corelib/core/BSDF.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace corelib.core
+{
+    enum BxDFType
+    {
+        BSDF_REFLECTION = 1 << 0,
+        BSDF_TRANSMISSION = 1 << 1,
+        BSDF_DIFFUSE = 1 << 2,
+        BSDF_GLOSSY = 1 << 3,
+        BSDF_SPECULAR = 1 << 4,
+
+        BSDF_ALL_TYPES = BSDF_DIFFUSE |
+                                BSDF_GLOSSY |
+                                BSDF_SPECULAR,
+
+        BSDF_ALL_REFLECTION = BSDF_REFLECTION |
+                                BSDF_ALL_TYPES,
+
+        BSDF_ALL_TRANSMISSION = BSDF_TRANSMISSION |
+                                BSDF_ALL_TYPES,
+
+        BSDF_ALL = BSDF_ALL_REFLECTION |
+                                BSDF_ALL_TRANSMISSION
+    }
+    class BSDF
+    {
+    }
+
+    class BxDF
+    {
+
+    }
+}
---
+++ .vs/corelib/corelib/core/BSDFFunction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace corelib.core
+{
+    public static class BSDFFunction 
+    {
+
+        #region shading coordinate 转换
+
+        /// <summary>
+        /// 计算w对应的cosθ
+        /// </summary>
+        /// <param name="w"></param>
+        /// <returns></returns>
+        public static float CosTheta(Vector w) { return w.z; }
+        public static float AbsCosTheta(Vector w) { return (float)Math.Abs(w.z); }
+        /// <summary>
+        /// 计算w对应的sin2θ
+        /// </summary>
+        /// <param name="w"></param>
+        /// <returns></returns>
+        public static float SinTheta2(Vector w) { return (float)Math.Max(0.0, 1.0 - CosTheta(w) * CosTheta(w)); }
+        public static float SinTheta(Vector w) { return (float)Math.Sqrt(SinTheta2(w)); }
+
+
+        public static float CosPhi(Vector w)
+        {
+            float sintheta = SinTheta(w);
+            if (sintheta == 0.0f) return 1.0f;//垂直
+            return LR.Clamp(w.x / sintheta, -1.0f, 1.0f);
+        }
+
+        public static float SinPhi(Vector w)
+        {
+            float sintheta = SinTheta(w);
+            if (sintheta == 0.0f) return 0.0f;
+                return LR.Clamp(w.y / sintheta, -1.0f, 1.0f);
+        }
+        #endregion
+
+        public static bool SameHemisphere(Vector w, Vector wp) {
+    return w.z* wp.z > 0.0f;
+    }
+
+}
+}
